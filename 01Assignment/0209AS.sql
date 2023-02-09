1e434c16ba4249d966e958d65760494132b05b6e
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-02-09 14:36:43 +0900

    ADD 0209 AS

---
+++ 01Assignment/0209AS.sql
@@ -0,0 +1,77 @@
+-- #1
+SELECT
+	*
+FROM
+	employees;
+
+-- #2
+SELECT
+	customerNumber,
+    customerName,
+    phone
+FROM
+	customers;
+
+-- #3
+SELECT
+	firstName,
+    lastName,
+    email
+FROM
+	employees
+ORDER BY
+	firstName;
+
+-- #4
+SELECT
+	firstName AS '이름',
+    lastName AS '성',
+    email AS '이메일'
+FROM
+	employees
+ORDER BY
+	이름;
+
+-- #5
+SELECT
+	employeeNumber,
+    lastName,
+    firstName,
+    officeCode,
+    jobTitle
+FROM
+	employees
+ORDER BY
+	jobTitle DESC;
+
+-- #6
+SELECT
+	*
+FROM
+	orderdetails
+ORDER BY
+	quantityOrdered,
+    priceEach;
+    
+-- #7
+SELECT
+	customerNumber,
+    country,
+    contactFirstName
+FROM
+	customers
+ORDER BY
+	country,
+    contactFirstName DESC;
+    
+-- #8
+SELECT
+	productCode,
+    quantityInStock,
+    buyPrice,
+    (quantityInStock * buyPrice)
+FROM
+	products
+ORDER BY
+	(quantityInStock * buyPrice) DESC;
+
